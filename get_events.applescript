054bd9b87e3756821afbac645fcdbd34bcf09ce8
reemhd <reemhde@gmail.com>
2023-10-21 20:42:27 +0100

    Authentication failing

--- get_events.applescript
+++
@@ -1,4 +0,0 @@
-tell application "Calendar"
-    set todayEvents to summary of events of calendar "Work" whose start date ≥ (current date) and start date < ((current date) + 1 * days)
-end tell
-return todayEvents
--- script.py
+++ script.py
@@ -1,23 +1,47 @@
-from dotenv import load_dotenv
+from pyicloud import PyiCloudService
+from pyicloud.exceptions import PyiCloudFailedLoginException
+from datetime import datetime, timedelta
 import os
-import subprocess
+from dotenv import load_dotenv
+import sys
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Read EVENT_TITLE from .env
-search_title = os.getenv("EVENT_TITLE")
+# Read username and password from environment variables
+username = os.getenv('APPLE_ID')
+password = os.getenv('APPLE_PASSWORD')
+event_title = os.getenv('EVENT_TITLE')
 
-def get_today_events():
-    script_path = "./get_events.applescript"
-    output = subprocess.check_output(["osascript", script_path])
-    event_titles = output.decode("utf-8").strip().split(", ")
-    return event_titles
+try:
+    # Authenticate
+    api = PyiCloudService(username, password)
+    if api.requires_2fa:
+        print("Two-factor authentication required.")
+        code = input("Enter the code you received: ")
+        result = api.validate_2fa_code(code)
+        print("Code validation result: %s" % result)
 
-def count_events_by_title(event_titles, search_title):
-    return event_titles.count(search_title)
+        if not result:
+            print("Failed to verify security code")
+            sys.exit(1)
 
-if __name__ == "__main__":
-    event_titles = get_today_events()
-    count = count_events_by_title(event_titles, search_title)
-    print(f"There are {count} events with the title '{search_title}' today.")
+except PyiCloudFailedLoginException:
+    print("Failed to log in to iCloud.")
+    exit(1)
+
+# Define the date range for events
+from_dt = datetime(2023, 9, 1, 0, 0, 0)
+to_dt = from_dt + timedelta(days=30)  # Next 30 days
+
+# Fetch the events
+events = api.calendar.events(from_dt, to_dt)
+
+# Count the number of events with a given title
+count = 0
+for event in events:
+    if event['title'] == event_title:
+        count += 1
+
+print(f"Number of events with title '{event_title}': {count}")
+
